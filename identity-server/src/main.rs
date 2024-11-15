3b7e57fd0c49771697f5f842d226c4ff7131cb29
Ryan Butler <thebutlah@gmail.com>
2024-11-15 02:44:30 -0500

    feat:  Implement config auto-generation and docker volumes (#18)

--- identity-server/src/main.rs
+++ identity-server/src/main.rs
@@ -1,15 +1,20 @@
 use std::{io::IsTerminal as _, path::PathBuf};
 
 use clap::Parser as _;
-use color_eyre::eyre::{bail, Context, OptionExt, Result};
+use color_eyre::{
+	eyre::{bail, Context, OptionExt, Result},
+	Section as _,
+};
 use futures::FutureExt;
-use tokio::sync::oneshot;
 use tokio::task::JoinHandle;
+use tokio::{io::AsyncWriteExt as _, sync::oneshot};
 use tracing::{debug, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use identity_server::{
-	config::{Config, DatabaseConfig, TlsConfig},
+	config::{
+		Config, DatabaseConfig, TlsConfig, ValidationError, DEFAULT_CONFIG_CONTENTS,
+	},
 	jwks_provider::JwksProvider,
 	spawn_http_server, spawn_https_server, MigratedDbPool,
 };
@@ -19,8 +24,120 @@ const GOOGLE_CLIENT_ID_DOCS_URL: &str = "https://developers.google.com/identity/
 #[derive(clap::Parser, Debug)]
 #[clap(version)]
 struct Cli {
+	#[clap(subcommand)]
+	command: Commands,
+}
+
+#[derive(clap::Parser, Debug)]
+enum Commands {
+	Serve(ServeArgs),
+	DefaultConfig(DefaultConfigArgs),
+}
+
+/// Runs the server
+#[derive(clap::Parser, Debug)]
+struct ServeArgs {
 	#[clap(long, env)]
 	config: PathBuf,
+}
+
+impl ServeArgs {
+	async fn run(self) -> Result<()> {
+		let cli = self;
+
+		let config_file = tokio::fs::read_to_string(&cli.config)
+			.await
+			.wrap_err("failed to read config file")
+			.with_note(|| format!("Config file path: {}", cli.config.display()))?;
+		debug!(contents = config_file, "config file contents");
+		let config_file: Config =
+			config_file.parse().wrap_err("config file was invalid")?;
+
+		let validation_result = config_file.validate();
+		if let Err(ref err) = validation_result {
+			let suggestion = match err {
+				ValidationError::UnspecifiedDomain => {
+					"try adding your domain in the `http.tls.domains` list"
+				}
+			};
+			validation_result
+				.wrap_err("config file was invalid")
+				.suggestion(suggestion)?;
+		}
+
+		let db_pool = {
+			let DatabaseConfig::Sqlite { ref db_file } = config_file.database;
+			let connect_opts = sqlx::sqlite::SqliteConnectOptions::new()
+				.create_if_missing(true)
+				.filename(db_file);
+			let pool_opts = sqlx::sqlite::SqlitePoolOptions::new();
+			let pool = pool_opts
+				.connect_with(connect_opts.clone())
+				.await
+				.wrap_err_with(|| {
+					format!(
+						"failed to connect to database with path {}",
+						connect_opts.get_filename().display()
+					)
+				})?;
+			MigratedDbPool::new(pool)
+				.await
+				.wrap_err("failed to migrate db pool")?
+		};
+		let reqwest_client = reqwest::Client::new();
+
+		let v1_cfg = identity_server::v1::RouterConfig {
+			uuid_provider: Default::default(),
+			db_pool,
+		};
+		let oauth_cfg = identity_server::oauth::OAuthConfig {
+			google_client_id: config_file
+				.third_party
+				.google
+				.clone()
+				.ok_or_eyre(format!(
+					"currently, setting up google is required. Please follow the \
+                instructions at {GOOGLE_CLIENT_ID_DOCS_URL} and fill in the \
+                `third_party.google.oauth2_client_id` field in the config.toml",
+				))?
+				.oauth2_client_id,
+			google_jwks_provider: JwksProvider::google(reqwest_client.clone()),
+		};
+		let router = identity_server::RouterConfig {
+			v1: v1_cfg,
+			oauth: oauth_cfg,
+		}
+		.build()
+		.await
+		.wrap_err("failed to build router")?;
+
+		let cache_dir = config_file.cache.dir();
+		debug!("using cache dir {}", cache_dir.display());
+		// .join(if cli.prod_tls { "prod" } else { "dev" });
+		tokio::fs::create_dir_all(&cache_dir)
+			.await
+			.wrap_err("failed to create cache directory for certs")?;
+
+		Tasks::spawn(config_file, router)
+			.await
+			.wrap_err("failed to spawn tasks")?
+			.run()
+			.await
+	}
+}
+
+/// Echoes the default config to stdout
+#[derive(clap::Parser, Debug)]
+struct DefaultConfigArgs {}
+
+impl DefaultConfigArgs {
+	async fn run(self) -> Result<()> {
+		tokio::io::stdout()
+			.write_all(DEFAULT_CONFIG_CONTENTS.as_bytes())
+			.await
+			.expect("should never fail");
+		Ok(())
+	}
 }
 
 /// Convenient container to manager all tasks that need to be monitored and reaped.
@@ -41,7 +158,7 @@ impl Tasks {
 			} else {
 				let tuple = spawn_https_server(config_file, router)
 					.await
-					.wrap_err("failed to spawn http server")?;
+					.wrap_err("failed to spawn https server")?;
 				(tuple.0, tuple.1)
 			};
 
@@ -74,6 +191,14 @@ impl Tasks {
 	}
 }
 
+fn is_root() -> bool {
+	#[cfg(unix)]
+	let result = rustix::process::getuid().is_root();
+	#[cfg(windows)]
+	let result = false;
+	result
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
 	color_eyre::install()?;
@@ -86,82 +211,13 @@ async fn main() -> Result<()> {
 		bail!("You should only run this program as a non-root user");
 	}
 
-	if std::io::stdout().is_terminal() {
+	if !std::io::stdout().is_terminal() {
 		debug!("We don't appear to be in a terminal");
 	}
 
 	let cli = Cli::parse();
-
-	let config_file = tokio::fs::read_to_string(cli.config)
-		.await
-		.wrap_err("failed to read config file")?;
-	let config_file: Config =
-		config_file.parse().wrap_err("config file was invalid")?;
-
-	let db_pool = {
-		let DatabaseConfig::Sqlite { ref db_file } = config_file.database;
-		let connect_opts = sqlx::sqlite::SqliteConnectOptions::new()
-			.create_if_missing(true)
-			.filename(db_file);
-		let pool_opts = sqlx::sqlite::SqlitePoolOptions::new();
-		let pool = pool_opts
-			.connect_with(connect_opts.clone())
-			.await
-			.wrap_err_with(|| {
-				format!(
-					"failed to connect to database with path {}",
-					connect_opts.get_filename().display()
-				)
-			})?;
-		MigratedDbPool::new(pool)
-			.await
-			.wrap_err("failed to migrate db pool")?
-	};
-	let reqwest_client = reqwest::Client::new();
-
-	let v1_cfg = identity_server::v1::RouterConfig {
-		uuid_provider: Default::default(),
-		db_pool,
-	};
-	let oauth_cfg = identity_server::oauth::OAuthConfig {
-		google_client_id: config_file
-			.third_party
-			.google
-			.clone()
-			.ok_or_eyre(format!(
-				"currently, setting up google is required. Please follow the \
-                instructions at {GOOGLE_CLIENT_ID_DOCS_URL} and fill in the \
-                `third_party.google.oauth2_client_id` field in the config.toml",
-			))?
-			.oauth2_client_id,
-		google_jwks_provider: JwksProvider::google(reqwest_client.clone()),
-	};
-	let router = identity_server::RouterConfig {
-		v1: v1_cfg,
-		oauth: oauth_cfg,
+	match cli.command {
+		Commands::Serve(args) => args.run().await,
+		Commands::DefaultConfig(args) => args.run().await,
 	}
-	.build()
-	.await
-	.wrap_err("failed to build router")?;
-
-	let cache_dir = config_file.cache.dir();
-	debug!("using cache dir {}", cache_dir.display());
-	// .join(if cli.prod_tls { "prod" } else { "dev" });
-	tokio::fs::create_dir_all(&cache_dir)
-		.await
-		.wrap_err("failed to create cache directory for certs")?;
-
-	Tasks::spawn(config_file, router)
-		.await
-		.wrap_err("failed to spawn tasks")?
-		.run()
-		.await
-}
-
-fn is_root() -> bool {
-	#[cfg(unix)]
-	let result = rustix::process::getuid().is_root();
-	#[cfg(windows)]
-	let result = false;
-	result
 }
