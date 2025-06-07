e8e92499d34bfe6b994978065a29e17dcd6ccbaf
Ryan Butler <thebutlah@gmail.com>
2025-06-07 05:03:39 -0400

    did most of TXT record to DidDocument conversion

--- did-pkarrm/src/resolver.rs
+++ did-pkarrm/src/resolver.rs
@@ -1,12 +1,14 @@
+use std::{collections::HashMap, str::FromStr};
+
 use bitflags::bitflags;
 use fluent_uri::Uri;
 use pkarr::{dns::rdata::TXT, SignedPacket};
 
 /// A verification method most typically is a public key (via `did:key`), or a Did Url
 /// that links to a verification method in a different Did Document.
-pub enum VerificationMethod<T = String> {
+pub enum VerificationMethod {
 	/// A `did:key`. This does not include the fragment suffix, to save space.
-	DidKey(Uri<T>),
+	DidKey(Did),
 	/// A reference to a verification method in a remote Did Document. Any method other
 	/// than `did:key` can be used.
 	///
@@ -14,7 +16,47 @@ pub enum VerificationMethod<T = String> {
 	/// parties or with alternative did methods such as did:web. By referencing external
 	/// Dids, users can use more convenient third party services while retaining their
 	/// ability for credible exit.
-	DidUrl(Uri<T>),
+	DidUrl(Did),
+}
+
+impl FromStr for VerificationMethod {
+	type Err = ParseVerificationMethodErr;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let uri: Uri<String> = Uri::from_str(s)?;
+		let did = Did::try_from(uri)?;
+		Ok(Self::from(did))
+	}
+}
+
+impl From<Did> for VerificationMethod {
+	fn from(value: Did) -> Self {
+		todo!()
+	}
+}
+
+pub struct Did(Uri<String>);
+
+#[derive(Debug, thiserror::Error)]
+pub enum DidFromUriErr {
+	#[error("did not start with did:")]
+	WrongPrefix,
+}
+
+impl TryFrom<Uri<String>> for Did {
+	type Error = DidFromUriErr;
+
+	fn try_from(value: Uri<String>) -> Result<Self, Self::Error> {
+		todo!()
+	}
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseVerificationMethodErr {
+	#[error("not a uri: {0}")]
+	NotAUri(#[from] fluent_uri::error::ParseError),
+	#[error("did not start with did:")]
+	NotADid(#[from] DidFromUriErr),
 }
 
 bitflags! {
@@ -54,17 +96,30 @@ bitflags! {
 	}
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum ParseVerificationRelationshipErr {
+	//TODO
+}
+
+impl FromStr for VerificationRelationship {
+	type Err = ParseVerificationRelationshipErr;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		todo!()
+	}
+}
+
 /// Everything in a did:pkarrm's Did Document except the `id` field. A
 /// `DidDocumentContents` can be mapped 1:1 to a DNS txt record, for use in PKARR.
 ///
 /// The generics are simply to enable borrowed data, they can be `&str` or `String`.
 /// See [fluent_uri](fluent_uri) for more info.
-pub struct DidDocumentContents<T1 = String, T2 = String> {
+pub struct DidDocumentContents {
 	/// "Also Known As". A list of alternative aliases for the user.
 	/// <https://www.w3.org/TR/cid-1.0/#also-known-as>
-	pub aka: Vec<Uri<T1>>,
+	pub aka: Vec<Uri<String>>,
 	/// The [VerificationMethod]s. Index in vec matches `vr`.
-	pub vm: Vec<VerificationMethod<T2>>,
+	pub vm: Vec<VerificationMethod>,
 	/// The [VerificationRelationship]s. The index in the vec matches
 	/// `vm`.
 	pub vr: Vec<VerificationRelationship>,
@@ -72,44 +127,79 @@ pub struct DidDocumentContents<T1 = String, T2 = String> {
 
 #[derive(Debug, thiserror::Error)]
 #[error("failed to extract information from dns txt record")]
-pub struct FromTxtRecordErr;
+pub enum FromTxtRecordErr {
+	#[error("encountered too many attributes")]
+	TooManyAttrs,
+	#[error("failed to extract fields from attributes: {0}")]
+	AttrsToFields(#[from] AttrsToFieldsErr),
+	#[error("failed to parse aka string: {0}")]
+	AkaParseErr(fluent_uri::error::ParseError),
+	#[error("failed to parse vm string: {0}")]
+	VmParseErr(#[from] ParseVerificationMethodErr),
+	#[error("failed to parse vr string: {0}")]
+	VrParseErr(#[from] ParseVerificationRelationshipErr),
+}
 
-impl<'a> TryFrom<TXT<'a>> for DidDocumentContents<&'a str, &'a str> {
+impl TryFrom<TXT<'_>> for DidDocumentContents {
 	type Error = FromTxtRecordErr;
 
-	fn try_from(value: TXT<'a>) -> Result<Self, Self::Error> {
+	fn try_from(value: TXT<'_>) -> Result<Self, Self::Error> {
 		let attrs = value.attributes();
-		for (k, v) in attrs.iter() {}
+		if attrs.len() >= usize::from(u8::MAX) {
+			return Err(FromTxtRecordErr::TooManyAttrs);
+		}
+		let fields = attrs_to_fields(&attrs)?;
 
-		let result = Self {
-			aka: todo!(),
-			vm: todo!(),
-			vr: todo!(),
-		};
+		let aka: Result<Vec<Uri<String>>, _> = fields
+			.aka
+			.into_iter()
+			.map(|s: &str| {
+				Uri::from_str(s).map_err(|e| FromTxtRecordErr::AkaParseErr(e))
+			})
+			.collect();
+		let aka = aka?;
+		let vm: Result<Vec<VerificationMethod>, _> = fields
+			.vm
+			.into_iter()
+			.map(|s: &str| {
+				VerificationMethod::from_str(s)
+					.map_err(|e| FromTxtRecordErr::VmParseErr(e))
+			})
+			.collect();
+		let vm = vm?;
+		let vr: Result<Vec<VerificationRelationship>, _> = fields
+			.vr
+			.into_iter()
+			.map(|s: &str| {
+				VerificationRelationship::from_str(s)
+					.map_err(|e| FromTxtRecordErr::VrParseErr(e))
+			})
+			.collect();
+		let vr = vr?;
 
-		todo!()
+		Ok(Self { aka, vm, vr })
 	}
 }
 
-enum Field {
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum Field {
 	Aka,
 	Vm,
 	Vr,
 }
 
+/// The index in the field's vec
 struct FieldIdx(u8);
 
 #[derive(Debug, thiserror::Error)]
-enum ParseFieldErr {
-	#[error("unknown field name")]
-	UnknownField,
-	#[error("field suffix was not a u8: {0}")]
+pub enum ParseKeyErr {
+	#[error("key suffix was not a u8: {0}")]
 	SuffixNotANumber(#[from] std::num::ParseIntError),
-	#[error("expected a suffix after the field name but none was provided")]
-	SuffixMissing,
+	#[error("unknown attribute key")]
+	UnknownKey,
 }
 
-fn parse_field_from_key(key: &str) -> Result<(Field, FieldIdx), ParseFieldErr> {
+fn parse_field_from_key(key: &str) -> Result<(Field, FieldIdx), ParseKeyErr> {
 	let (field, suffix) = if let Some(suffix) = key.strip_prefix("aka") {
 		(Field::Aka, suffix)
 	} else if let Some(suffix) = key.strip_prefix("vm") {
@@ -117,14 +207,104 @@ fn parse_field_from_key(key: &str) -> Result<(Field, FieldIdx), ParseFieldErr> {
 	} else if let Some(suffix) = key.strip_prefix("vr") {
 		(Field::Vr, suffix)
 	} else {
-		return Err(ParseFieldErr::UnknownField);
+		return Err(ParseKeyErr::UnknownKey);
 	};
 
-	let idx: u8 = match field {
-		Field::Aka => suffix.parse()?,
-		Field::Vm => todo!(),
-		Field::Vr => todo!(),
-	};
+	let idx: u8 = suffix.parse()?;
+	Ok((field, FieldIdx(idx)))
+}
 
-	todo!()
+/// The sum of a arithmetic sequence of numbers:
+/// `0, 1, 2, .. N`
+///
+/// It is equal to:
+/// `len(sequence) * (min(sequence) + max(sequence)) / 2`
+/// aka `(N + 1)*N/2`
+fn arithmetic_series(max: u8) -> u16 {
+	// because we start at 0, we add 1 to the max to get the length of the sequence
+	let count = u16::from(max) + 1;
+	count
+		.checked_mul(u16::from(max))
+		.unwrap()
+		.checked_div(2)
+		.unwrap()
+}
+
+#[derive(Default)]
+struct Fields<T> {
+	aka: T,
+	vm: T,
+	vr: T,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AttrsToFieldsErr {
+	#[error("attribute value was empty string or not present")]
+	EmptyVal,
+	#[error("failed to parse attribute key: {0}")]
+	ParseKey(#[from] ParseKeyErr),
+	#[error("skipped an index for field {0:?}")]
+	SkippedAttrIdx(Field),
+}
+
+// Get ready for the most overengineered code ever...
+fn attrs_to_fields(
+	attrs: &HashMap<String, Option<String>>,
+) -> Result<Fields<Vec<&str>>, AttrsToFieldsErr> {
+	// We can detect gaps without allocating by ensuring that the sum of all keys
+	// is equal to len(seq)*max(seq)/2 and that min(seq) == 0.
+	//
+	// (this is just the formula for a finite arithmetic series)
+	#[derive(Default)]
+	struct Stats {
+		max: u8,
+		min: u8,
+		sum: u16,
+		count: u8,
+	}
+	impl Stats {
+		fn was_index_skipped(&self) -> bool {
+			self.min != 0
+				|| self.count != (self.max + 1)
+				|| self.sum != arithmetic_series(self.max)
+		}
+	}
+
+	let mut fstats: Fields<Stats> = Default::default();
+	let mut fvalues: Fields<Vec<&str>> = Default::default();
+	for (k, v) in attrs.iter() {
+		let v = v.as_deref().unwrap_or_default();
+		if v.is_empty() {
+			return Err(AttrsToFieldsErr::EmptyVal);
+		}
+
+		let (field, idx) = parse_field_from_key(k)?;
+		let (stats, values) = match field {
+			Field::Aka => (&mut fstats.aka, &mut fvalues.aka),
+			Field::Vm => (&mut fstats.vm, &mut fvalues.vm),
+			Field::Vr => (&mut fstats.vr, &mut fvalues.vr),
+		};
+		stats.max = u8::max(stats.max, idx.0);
+		stats.min = u8::min(stats.min, idx.0);
+		stats.sum += u16::from(idx.0);
+		stats.count += 1;
+		values.resize(stats.count.into(), ""); // guarantees next command wont fail
+		values[usize::from(idx.0)] = v;
+	}
+
+	if fstats.aka.was_index_skipped() {
+		return Err(AttrsToFieldsErr::SkippedAttrIdx(Field::Aka));
+	}
+	if fstats.vm.was_index_skipped() {
+		return Err(AttrsToFieldsErr::SkippedAttrIdx(Field::Vm));
+	}
+	if fstats.vr.was_index_skipped() {
+		return Err(AttrsToFieldsErr::SkippedAttrIdx(Field::Vr));
+	}
+	// sanity: we already ensured there are no holes
+	debug_assert!(!fvalues.aka.iter().any(|s| s.is_empty()), "sanity");
+	debug_assert!(!fvalues.vm.iter().any(|s| s.is_empty()), "sanity");
+	debug_assert!(!fvalues.vr.iter().any(|s| s.is_empty()), "sanity");
+
+	Ok(fvalues)
 }
