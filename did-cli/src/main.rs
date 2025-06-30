9196d82d06823f01dec529c132357229a0b1d10f
Ryan Butler <thebutlah@gmail.com>
2025-06-29 22:40:16 -0400

    feat(did-cli): create subcommand

--- did-cli/src/main.rs
+++ did-cli/src/main.rs
@@ -1,6 +1,9 @@
 use clap::{Parser, Subcommand};
-use color_eyre::Result;
+use color_eyre::{Result, Section};
+use did_cli::DidMethodKind;
 use did_common::did::Did;
+use ed25519_dalek::SigningKey;
+use eyre::{eyre, Context};
 use tracing::info;
 use tracing_subscriber::{
 	layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter,
@@ -18,6 +21,7 @@ fn main() -> Result<()> {
 	match args.subcommands {
 		Subcommands::Create(cmd) => cmd.run(),
 		Subcommands::Read(cmd) => cmd.run(),
+		Subcommands::Update(cmd) => cmd.run(),
 	}
 }
 
@@ -32,14 +36,34 @@ struct Args {
 enum Subcommands {
 	Create(CreateCmd),
 	Read(ReadCmd),
+	Update(UpdateCmd),
 }
 
 #[derive(Debug, Parser)]
-struct CreateCmd;
+struct CreateCmd {
+	kind: String,
+}
 
 impl CreateCmd {
 	fn run(self) -> Result<()> {
-		todo!()
+		let s = self.kind.trim();
+		let method = if s == "did:key" {
+			DidMethodKind::Key
+		} else if s == "did:pkarr" {
+			DidMethodKind::Pkarr
+		} else {
+			return Err(eyre!("unknown did method"))
+				.suggestion("try did:key or did:pkarr");
+		};
+		let client = did_cli::client::Client::builder().build();
+		let mut rand = rand::thread_rng();
+		let priv_key = SigningKey::generate(&mut rand);
+		let did = client
+			.create(method, &priv_key)
+			.wrap_err("failed to create did")?;
+		println!("{did}");
+
+		Ok(())
 	}
 }
 
@@ -55,5 +79,16 @@ impl ReadCmd {
 		println!("{doc:#?}");
 
 		Ok(())
+	}
+}
+
+#[derive(Debug, Parser)]
+struct UpdateCmd {
+	did: Did,
+}
+
+impl UpdateCmd {
+	fn run(self) -> Result<()> {
+		todo!()
 	}
 }
